9a4410ee0f813e9bbb9b08d4b83a108a18bcf829
Evan <pokevoyager0@gmail.com>
2020-09-29 15:44:51 -0600

    reset chain fishing counter on failed reeling

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1954,6 +1954,7 @@ static bool8 Fishing_StartEncounter(struct Task *task)
 
 static bool8 Fishing_NotEvenNibble(struct Task *task)
 {
+    gChainFishingStreak = 0;    //player failed to reel in a mon. reset chain fishing counter
     AlignFishingAnimationFrames();
     StartSpriteAnim(&gSprites[gPlayerAvatar.spriteId], GetFishingNoCatchDirectionAnimNum(GetPlayerFacingDirection()));
     FillWindowPixelBuffer(0, PIXEL_FILL(1));
@@ -1964,6 +1965,7 @@ static bool8 Fishing_NotEvenNibble(struct Task *task)
 
 static bool8 Fishing_GotAway(struct Task *task)
 {
+    gChainFishingStreak = 0;    //player failed to reel in a mon. reset chain fishing counter
     AlignFishingAnimationFrames();
     StartSpriteAnim(&gSprites[gPlayerAvatar.spriteId], GetFishingNoCatchDirectionAnimNum(GetPlayerFacingDirection()));
     FillWindowPixelBuffer(0, PIXEL_FILL(1));
