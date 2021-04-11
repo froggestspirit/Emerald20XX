69511c3d1f725c0d8c34a0d0ea4302dd99631a36
Seamus <seamus@pop-os.localdomain>
2021-04-11 19:37:36 -0400

    Physical/Special Split

--- include/pokemon.h
+++ include/pokemon.h
@@ -206,6 +206,10 @@ struct BaseStats
             u8 noFlip : 1;
 };
 
+#define MOVE_PHYSICALITY_PHYSICAL	0
+#define MOVE_PHYSICALITY_SPECIAL	1
+#define MOVE_PHYSICALITY_OTHER		2
+
 struct BattleMove
 {
     u8 effect;
@@ -217,6 +221,7 @@ struct BattleMove
     u8 target;
     s8 priority;
     u8 flags;
+    u8 physicality;
 };
 
 struct SpindaSpot
