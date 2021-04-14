b9eee3cc998c9ba5432ca8823fa2268bf88ed763
froggestspirit <froggestspirit@gmail.com>
2021-04-13 20:41:21 -0400

    NPC palette fix

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -1794,7 +1794,7 @@ static u8 LoadSpritePaletteIfTagExists(const struct SpritePalette *spritePalette
     if (IndexOfSpritePaletteTag(spritePalette->tag) != 0xFF)
         return 0xFF;
 
-    return LoadSpritePalette(spritePalette);
+    return LoadSpritePaletteDayNight(spritePalette);
 }
 
 void PatchObjectPalette(u16 paletteTag, u8 paletteSlot)
