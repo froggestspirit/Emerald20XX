c3b4c123a4abae5a7651a2a343b1300dcef57973
froggestspirit <froggestspirit@gmail.com>
2021-04-17 11:08:06 -0400

    Start with running shoes

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -637,7 +637,7 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
         return;
     }
 
-    if (!(gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER) && (heldKeys & B_BUTTON) && FlagGet(FLAG_SYS_B_DASH)
+    if (!(gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER) && (heldKeys & B_BUTTON)
      && IsRunningDisallowed(gObjectEvents[gPlayerAvatar.objectEventId].currentMetatileBehavior) == 0 && !FollowerComingThroughDoor())
     {
         PlayerRun(direction);
