a527a1f565a2922f50e57fb2fd4452b145adc3dd
froggestspirit <froggestspirit@gmail.com>
2021-08-17 22:52:29 -0400

    trainer card fix

--- src/trainer_pokemon_sprites.c
+++ src/trainer_pokemon_sprites.c
@@ -391,7 +391,7 @@ u16 sub_818D904(u16 species, bool8 isFrontPic, u8 paletteSlot, u8 windowId)
 
 u16 CreateTrainerCardTrainerPicSprite(u16 species, bool8 isFrontPic, u16 destX, u16 destY, u8 paletteSlot, u8 windowId)
 {
-    return CreateTrainerCardSprite(species, 0, 0, isFrontPic, destX, destY, paletteSlot, windowId, TRUE);
+    return CreateTrainerCardSprite(species, 0, 0xFFFF, isFrontPic, destX, destY, paletteSlot, windowId, TRUE);
 }
 
 u16 PlayerGenderToFrontTrainerPicId_Debug(u8 gender, bool8 getClass)
